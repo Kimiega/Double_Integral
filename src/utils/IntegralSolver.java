1e5a48aa40749fed650e6abcde24a58141dd07e7
Kimiega <93886405+Kimiega@users.noreply.github.com>
2023-01-17 18:03:24 +0300

    Update IntegralSolver.java

--- src/utils/IntegralSolver.java
+++ src/utils/IntegralSolver.java
@@ -6,12 +6,16 @@ import models.Polygon;
 import java.util.List;
 
 public class IntegralSolver {
+    private Dot leftBottomDotBeam;
+    private Dot rightUpperDotBeam;
     private Polygon polygon;
     private Long fineness;
 
-    public IntegralSolver(Long fineness, Polygon polygon) {
+    public IntegralSolver(Long fineness, Polygon polygon, Dot leftUpperDotBeam, Dot rightBottomDotBeam) {
         this.fineness = fineness;
         this.polygon = polygon;
+        this.leftBottomDotBeam = leftUpperDotBeam;
+        this.rightUpperDotBeam = rightBottomDotBeam;
     }
 
     private Double function(double x, double y) {
@@ -24,6 +28,9 @@ public class IntegralSolver {
         Double b = polygon.getEndSmallDot(fineness, center).getX();
         Double c = polygon.getStartSmallDot(fineness, center).getY();
         Double d = polygon.getEndSmallDot(fineness, center).getY();
+        if (a< leftBottomDotBeam.getX() || b> rightUpperDotBeam.getX() ||
+                c< leftBottomDotBeam.getY() || d> rightUpperDotBeam.getY())
+            return Math.abs(function(x,y)/2);
         return Math.abs((b-a)*(b-a)*y*(2*Math.cos(x*y)-x*y*Math.sin(x*y))-(d-c)*(d-c)/x/x/x*Math.sin(x*y));
     }
     public Double integrate() {
